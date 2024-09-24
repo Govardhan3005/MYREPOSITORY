ec652fc88fb767acd61eced230a886c1cfbb3c97
GOVARDHAN N <GOVARDHAN192004@gmail.com>
2024-09-24 23:04:32 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Original array
+original_array = np.array([1, 2, 3, 4])
+
+print("Original array:")
+print(original_array)
+
+# Convert array to floating type
+float_array = original_array.astype(float)
+
+print("\nArray converted to a float type:")
+print(float_array)
